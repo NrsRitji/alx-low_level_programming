f28e5e249ad9b3aa7edc10f963d91231cf8e0816
Ritji Ishaku <ritjimessiah@gmail.com>
2022-10-14 18:00:42 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,18 +4,18 @@
 
 /**
  * main - print if the number is postive, zero, or negative
- * *
- * * Description: using the main function
- * * this program prints "Programming is positive, zero, or negative
- * * Return: 0
- * */
+ *
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	  /*Code goes below here */
+	  /*Code goes below here*/
 	if (n > 0)
 	{
 			printf("%i is positive\n", n);
--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,25 +1,14 @@
 #include <stdio.h>
-
 /**
  * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
  */
 int main(void)
-
 {
-
-		char letter;
-
-		for (letter = 'a'; letter <= 'z'; letter++)
-
-					putchar(letter);
-
-
-			putchar('\n');
-
-
-
-				return (0);
-
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
 }
