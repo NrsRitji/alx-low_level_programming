c05e53e52b849ee7ab32b328cdba620822fcebb4
Ritji Ishaku <ritjimessiah@gmail.com>
2022-10-18 21:17:20 +0100

    i am strong

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,10 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 - print 10 times the alphabet in lowercase
+ * print_alphabet_x10 - print the alphabet in lowercase
  */
 
-void void print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 	int count = 0
 	char letter;
@@ -14,4 +15,5 @@ void void print_alphabet_x10(void)
 		_putchar(letter);
 		_putchar('\n');
 	}
+}
 
