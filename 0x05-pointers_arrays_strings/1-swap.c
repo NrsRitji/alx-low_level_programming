d94805477e5ac442471289adb24fabd90d144b14
Ritji Ishaku <ritjimessiah@gmail.com>
2022-10-24 09:48:18 +0100

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,18 @@
 #include "main.h" 
- /** 
- * swap_int - swaps the value of int a and int b 
- * Owned by Bwave/Bright 
- * @a: first int 
- * @b: second int 
- * Return 0 
- */ 
- void swap_int(int *a, int *b) 
- { 
-                 int s; 
-  
-                 s = *a; 
-                 *a = *b; 
-                 *b = s; 
- }
+ /**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int a;
+    int b;
+
+    a = 98;
+    b = 42;
+    printf("a=%d, b=%d\n", a, b);
+    swap_int(&a, &b);
+    printf("a=%d, b=%d\n", a, b);
+    return (0);
+}
