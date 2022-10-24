6df5b5fd26903bc3e92eca808515c4d2f15d6668
Ritji Ishaku <ritjimessiah@gmail.com>
2022-10-24 16:07:03 +0100

    ritji

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,14 @@
-#include "main.h" 
+#include "main.h"
+
 /**
- * _puts - prints a string 
- * @str: the string 
- */ 
- void _puts(char *str) 
+ * _puts - prints a string
+ * @str: the string
+ */
+ void _puts(char *str)
  {
-         for (; *str != '\0';str++) 
-         { 
+	  for (; *str != '\0';str++)
+         {
                 _putchar(*str);
-         } 
-         _putchar('\n'); 
+         }
+         _putchar('\n');
  }
