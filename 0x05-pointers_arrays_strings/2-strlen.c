fb5bd65b40b5979eec2c9ff105e1405afbb0bd10
Ritji Ishaku <ritjimessiah@gmail.com>
2022-10-24 14:39:04 +0100

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <string.h>
-/** 
-* _strlen - return the length of a string 
-* @s: char to check 
+/**
+*_strlen - return the length of a string 
+*@s: char to check 
 * Return: return length of this string
 */
 
- int _strlen(char *s)
+int _strlen(char *s)
 {
 	return (strlen(s));
 }
