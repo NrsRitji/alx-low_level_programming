70268e8138ea4ca485c929d5c6ca4cb4dd188eb3
Ritji Ishaku <ritjimessiah@gmail.com>
2022-10-24 11:45:56 +0100

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * reset_to_98 - a function to update the reference of a pointer
+ * reset_to_98 - a function to update the reference of a pointer
  *  @n: pointer to n to be updated
  *  Owned By NrsRitji
  *  Return: 0 is success
