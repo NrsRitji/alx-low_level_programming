b4dfb713b641152157c7603f994ef9c96f19de7b
Ritji Ishaku <ritjimessiah@gmail.com>
2022-10-24 14:01:43 +0100

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,19 @@
-#include "main.h" 
+#include "main.h"
+
  /** 
   * _strlen - return the length of a string 
-  * Owned By Bwave 
   * @s: char to check 
-  * Description: this will return the length of a string 
-  * Return: 0 is success 
-  */ 
- int _strlen(char *s) 
- { 
-         int a = 0; 
-  
-         for (; *s++;) 
-                 a++; 
-         return (a); 
- }
+  * Return: return length of this string
+  */
+
+ int _strlen(char *s)
+{
+	int len;
+
+	while(*s != '\0')
+	{
+		len += 1;
+		*s = *s + 1;
+	}
+	return (len);
+}
